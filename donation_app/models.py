19972f6d3037247f8022428800dc1207655f6c69
Maciej Koczot <mefiuea@gmail.com>
2022-05-19 15:29:33 +0200

    upgrade models, templates, add counting bags and institutions to main page

--- donation_app/models.py
+++ donation_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.core.validators import RegexValidator, MaxLengthValidator
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+# Validators
+phone_regex = RegexValidator(regex=r'(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)')
+zip_code_regex = RegexValidator(regex=r'\d{2}-\d{3}')
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=200, unique=True, blank=False, verbose_name='Nazwa')
+
+
+class InstitutionModel(models.Model):
+    name = models.CharField(max_length=200, unique=True, blank=False, verbose_name='Nazwa')
+    description = models.TextField(blank=False, validators=[MaxLengthValidator(2000)], verbose_name='Opis')
+    TYPE_CHOICES = [
+        ('F', 'Fundacja'),
+        ('OP', 'Organizacja Pozarządowa'),
+        ('ZL', 'Zbiórka Lokalna'),
+    ]
+    type = models.CharField(max_length=23, choices=TYPE_CHOICES, blank=False, default='F', verbose_name='Typ')
+    categories = models.ManyToManyField('CategoryModel', blank=False, related_name='institution_category')
+
+
+class DonationModel(models.Model):
+    quantity = models.PositiveIntegerField(blank=False, verbose_name='ilość')
+    categories = models.ManyToManyField('CategoryModel', blank=False, related_name='donation_category')
+    institution = models.ForeignKey('InstitutionModel', on_delete=models.PROTECT, related_name='institution')
+    address = models.CharField(max_length=200, blank=False, verbose_name='Adres')
+    phone_number = models.CharField(max_length=17, blank=False, validators=[phone_regex], verbose_name='Numer telefonu')
+    city = models.CharField(max_length=50, blank=False, verbose_name='Miasto')
+    zip_code = models.CharField(max_length=6, blank=False, validators=[zip_code_regex], verbose_name='Kod pocztowy')
+    pick_up_date = models.DateField(verbose_name='Data')
+    pick_up_time = models.TimeField(verbose_name='Godzina')
+    pick_up_comment = models.TextField(blank=True, null=True, validators=[MaxLengthValidator(300)], verbose_name='Opis')
+    user = models.ForeignKey(get_user_model(), blank=True, null=True, default=None, on_delete=models.PROTECT,
+                             related_name='donator')
--- home_app/views.py
+++ home_app/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from donation_app.models import DonationModel, InstitutionModel
+
 
 def landing_page_view(request):
     if request.method == 'GET':
-        return render(request, 'home_app/landing_page.html')
+        # total number of bags from all donations
+        sum_quantity_bags = 0
+        donations_objects = DonationModel.objects.all()
+        for donation in donations_objects:
+            sum_quantity_bags += donation.quantity
+
+        # number of supported organizations
+        number_of_institutions = InstitutionModel.objects.all().count()
+
+        context = {
+            'sum_of_bags': sum_quantity_bags,
+            'number_of_institutions': number_of_institutions,
+        }
+
+        return render(request, 'home_app/landing_page.html', context=context)
