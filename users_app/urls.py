bd166ce15624f22f1ef01b118551882e73dd5b80
Maciej Koczot <mefiuea@gmail.com>
2022-05-24 11:07:17 +0200

    add login options

--- users_app/urls.py
+++ users_app/urls.py
@@ -8,6 +8,6 @@ app_name = 'users_app'
 
 urlpatterns = [
     path('login/', views.login_view, name='login_view'),
-    # path('login/', v.LoginView.as_view(template_name='users_app/login.html', authentication_form=LoginForm), name='login_view'),
     path('register/', views.register_view, name='register_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.core.exceptions import ObjectDoesNotExist
 
 from .forms import RegistrationForm, LoginForm
 
@@ -12,7 +13,7 @@ def register_view(request):
             user = form.save(commit=False)
             user.is_active = True
             user.save()
-            print('PRINT: Uzytkownik utworzony!!!!', user, flush=True)
+            print('PRINT: Użytkownik utworzony!!!!', user, flush=True)
             return redirect('users_app:login_view')
         else:
             context = {'form': form, }
@@ -36,9 +37,23 @@ def login_view(request):
                 return redirect('users_app:register_view')
         else:
             print('Walidacja formularza nie przeszła', flush=True)
-            print('ERRORS:', form.errors, flush=True)
             context = {'form': form, }
-
-            return render(request, 'users_app/login.html', context=context)
+            # check if user email is in database
+            email_field = request.POST.get('username')
+            current_user_model = get_user_model()
+            # emails_from_db = current_user_model.objects.get(email=email_field)
+            # print('USERS: ', emails_from_db, flush=True)
+            try:
+                current_user_model.objects.get(email=email_field)
+                print('Taki mail jest w bazie danych', flush=True)
+                return render(request, 'users_app/login.html', context=context)
+            except ObjectDoesNotExist:
+                print('Takiego maila nie ma w bazie danych', flush=True)
+                return redirect('users_app:register_view')
 
     return render(request, 'users_app/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home_app:landing_page_view')
