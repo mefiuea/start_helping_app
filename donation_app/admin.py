4b1f9dd647149fdc8d095527f6caf6f9d3418a73
Maciej Koczot <mefiuea@gmail.com>
2022-06-03 12:16:28 +0200

    register new models in admin panel

--- donation_app/admin.py
+++ donation_app/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import InstitutionModel
+from .models import InstitutionModel, CategoryModel, DonationModel
 
 
 @admin.register(InstitutionModel)
 class InstitutionAdmin(admin.ModelAdmin):
     list_display = ('name', 'type')
+
+
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(DonationModel)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('user_donator', 'address')
